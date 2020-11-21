24202664af8199a806ee284d5b082391e692ddff
unknown <panagiotis.kostopanagiotis@gmail.com>
2020-11-21 16:30:45 +0200

    Added circuit logic

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ from quantum_operator import operator
 import numpy as np
 from math import sqrt
 
-#Test application
+#Test application - Entanglement
 
 cur = state( np.array( [ 0, 1 ] ) )
-op = operator( np.array( [ [ 1.0, 0.0], [ 0.0, 1.0 ] ] ) )
-cur.apply_operator( op )
-print( cur.get_state() )
+hadamard = operator( np.array( [ [ 1/sqrt(2), 1/sqrt(2) ], [ 1/sqrt( 2 ), -1/sqrt(2) ] ] ) )
+cur.apply_operator( hadamard )
+cur.add_state( state( [ 1, 0 ] ) )
 
-op.apply( operator( np.array( [ [ 1/sqrt(2), 1/sqrt(2) ], [ 1/sqrt( 2 ), -1/sqrt(2) ] ] ) ) )
-cur.apply_operator( op )
+cnot = operator( np.array( [ [ 1, 0, 0, 0 ], [ 0, 1, 0, 0 ], [ 0, 0, 0, 1 ], [0 ,0, 1, 0 ] ] ) )
 
-print( cur.get_state() )
-cur.add_state( state( np.array( [ 0, 1 ] ) ) )
-print( cur.get_state() )
-cur.add_state( state( np.array( [ 1/sqrt(2), 1/sqrt(2) ] ) ) )
-print( cur.measure() )
+cur.apply_operator( cnot )
+
 print( cur.evaluate_results() )
 cur.plot_results()
